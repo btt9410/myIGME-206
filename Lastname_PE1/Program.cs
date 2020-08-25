b024803a470a6c2743902038575c32ee9cfd724c
bryan <bryan@DESKTOP-6UQEVGN>
2020-08-25 15:41:28 -0400

    lastname program exericse

--- Lastname_PE1/Program.cs
+++ Lastname_PE1/Program.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-// Bryan Taber
-// 8/24/20
 namespace Lastname_PE1
 {
+    // Class: Program
+    // Author: Bryan Taber
+    // Purpose: Console Write and Integer Test
+    // Restrictions: None
     class Program
     {
+        // Method: Main
+        // Purpose: Test printing general phrase and name
+        //          Test adding and writing integers
+        // Restriction: None
         static void Main(string[] args)
         {
+            // Prints Hello World
             Console.WriteLine("Hello World!");
+            // Prints Name
             Console.WriteLine("Bryan Taber");
+            // Creating two integers and adding together, printing result
             int one = 1;
             int two = 2;
             int result = one + two; // Should print as 3
