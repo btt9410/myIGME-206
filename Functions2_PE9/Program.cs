0f9a9e155878b817d1f1b5a84332b64d1002f647
bryan <bryan@DESKTOP-6UQEVGN>
2020-09-09 18:41:48 -0400

    program exercise 9

---
+++ Functions2_PE9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions2_PE9
+{
+    class Program
+    {
+        delegate string ConsoleRead();
+        static void Main(string[] args)
+        {
+            ConsoleRead read;
+            read = new ConsoleRead(Console.ReadLine);
+        }
+    }
+}
