c0e071435b871826e7aca0192033ff1aaeeaa4c8
bryan <bryan@DESKTOP-6UQEVGN>
2020-09-15 18:45:27 -0400

    unit test 1

---
+++ UnitTestDelegate/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestDelegate
+{
+    // Class Program
+    // Author: Bryan Taber
+    // Purpose: Using delegate to call method
+    // Restrictions: None
+    class Program
+    {
+        delegate string ConsoleRead();
+        // Method: Main
+        // Purpose: Declare variable for delegate
+        //          Set variable to method Console.ReadLine()
+        //          Display user's value
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            ConsoleRead read;
+            // gives delegate the ReadLine() method
+            read = new ConsoleRead(Console.ReadLine);
+            Console.Write("Enter whatever you want >> ");
+            // sets the entry of the user to a stringt
+            string entry = read();
+            Console.WriteLine("You wrote: " + entry);
+        }
+    }
+}
