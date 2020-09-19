aa6e0e4bbb773c82a6b04d66e4e2337987b11397
bryan <bryan@DESKTOP-6UQEVGN>
2020-09-19 15:57:44 -0400

    progam exercise 11

---
+++ Traffic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Vehicles;
+
+namespace Traffic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void AddFunction(IPassengerCarrier passenger)
+            {
+                passenger.LoadPassenger();
+                Console.WriteLine(passenger.ToString());
+            }
+        }
+    }
+}
