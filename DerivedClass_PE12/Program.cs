f8158b2d968bae140df5de7da8195f04b806e097
bryan <bryan@DESKTOP-6UQEVGN>
2020-09-23 13:35:56 -0400

    program exercise 12

---
+++ DerivedClass_PE12/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DerivedClass_PE12
+{
+    public class MyDerivedClass : MyClass
+    {
+        public override string GetString()
+        {
+			return base.GetString() + " (output from the derived class)";
+        }
+		public static void Main(string[] args)
+        {
+			MyDerivedClass one = new MyDerivedClass();
+			Console.WriteLine(one.GetString());
+        }
+    }
+	public class MyClass
+	{
+		private string myString = "test";
+		public virtual string GetString()
+		{
+			return myString;
+		}
+	}
+
+}
