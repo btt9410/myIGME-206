3b6dac523169c21b77dbee66959ee396d4ebf596
bryan <bryan@DESKTOP-6UQEVGN>
2020-09-29 18:08:29 -0400

    program exercise 14

---
+++ Class_PE14/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_PE14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Cow cow = new Cow();
+			Crow crow = new Crow();
+			MyMethod(cow);
+			MyMethod(crow);
+        }
+		public static void MyMethod(object myObject)
+		{
+			IAnimal animal = (IAnimal)myObject;
+			animal.Noise();
+		}
+    }
+	public interface IAnimal
+	{
+		void Noise();
+	}
+	public class Cow : IAnimal
+	{
+		public void Noise()
+		{
+			Console.WriteLine("Moo!");
+		}
+	}
+	public class Crow : IAnimal
+	{
+		public void Noise()
+		{
+			Console.WriteLine("Ca-caw!");
+		}
+	}
+
+}
