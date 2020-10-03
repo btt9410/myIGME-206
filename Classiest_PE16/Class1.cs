598efa5b11ea3b468f68569cb11f0049f95d9e17
bryan <bryan@DESKTOP-6UQEVGN>
2020-10-03 15:10:21 -0400

    program exercise 16

---
+++ Classiest_PE16/Class1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classiest_PE16
+{
+    public interface IMood
+    {
+        string Mood { get; }
+    }
+    public abstract class HotDrink
+    {
+        public bool instant;
+        public bool milk;
+        public byte sugar;
+        public string size;
+        public Customer customer;
+
+        public virtual void AddSugar(byte amount)
+        {
+        }
+        public abstract void Steam();
+
+        public HotDrink()
+        {
+        }
+        public HotDrink(string brand)
+        {
+        }
+    }
+    public interface ITakeOrder
+    {
+        void TakeOrder();
+    }
+    public class Waiter : IMood
+    {
+        public string name;
+        public string Mood { get; }
+
+        public void ServeCustomer(HotDrink cup)
+        {
+        }
+    }
+    public class Customer : IMood
+    {
+        public string name;
+        public string creditCardNumber;
+
+        public string Mood { get; }
+    }
+    public class CupOfCoffee : HotDrink, ITakeOrder
+    {
+        public string beanType;
+
+        public override void Steam()
+        {
+        }
+        public void TakeOrder()
+        {
+        }
+
+        public CupOfCoffee(string brand) : base(brand)
+        {
+        }
+    }
+    public class CupOfTea : HotDrink, ITakeOrder
+    {
+        public string leafType;
+
+        public override void Steam()
+        {
+        }
+        public void TakeOrder()
+        {
+        }
+
+        public CupOfTea(bool customerIsWealthy)
+        {
+        }
+    }
+    public class CupOfCocoa : HotDrink, ITakeOrder
+    {
+        public static int numCups;
+        public bool marshmallows;
+        private string source;
+
+        public string Source 
+        { 
+            set
+            {
+            }
+        }
+        public override void Steam()
+        {
+        }
+        public override void AddSugar(byte amount)
+        {
+        }
+        public void TakeOrder()
+        {
+        }
+        
+        CupOfCocoa() : this(false)
+        {
+        }
+        CupOfCocoa(bool marshmallows) : base("Expensive Organic Brand")
+        {
+        }
+    }
+}
