204bbac889611d5aee22a1e20da25bd40404e5ac
bryan <bryan@DESKTOP-6UQEVGN>
2020-11-15 14:32:58 -0500

    unit test 3

---
+++ Adjacent_UT3/Program.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Adjacent_UT3
+{
+    class Program
+    {
+        static bool[,] graphMatrix = new bool[,]
+        { //         RED      BLUE     GREY    TEAL   YELLOW     ORANGE     PURPLE   GREEN
+    /* RED (0) */ {  false,  true,     true,  false,  false,     false,    false,   false },
+   /* BLUE (1) */ {  false,  false,   false,   true,   true,     false,    false,   false },
+   /* GREY (2) */ {  false,  false,   false,   true,  false,      true,    false,   false },
+   /* TEAL (3) */ {  false,   true,    true,  false,  false,     false,    false,   false },
+ /* YELLOW (4) */ {  false,  false,   false,  false,  false,     false,    false,    true },
+ /* ORANGE (5) */ {  false,  false,   false,  false,  false,     false,     true,   false },
+ /* PURPLE (6) */ {  false,  false,   false,  false,   true,     false,    false,   false },
+  /* GREEN (7) */ {  false,  false,   false,  false,  false,     false,    false,   false },
+        };
+
+        static int[][] graphList = new int[][]
+        {
+            new int[] { 1, 2 },
+            new int[] { 3, 4 },
+            new int[] { 3, 5 },
+            new int[] { 1, 2 },
+            new int[] { 7 },
+            new int[] { 6 },
+            new int[] { 4 },
+            null
+        };
+
+        static List<Node> path = new List<Node>();
+        static bool wait = false;
+        static int intState = 0;
+        static string userState;
+
+        static void Main(string[] args)
+        {
+            Node node;
+
+            node = new Node(0);
+            path.Add(node);
+
+            node = new Node(1);
+            path.Add(node);
+
+            node = new Node(2);
+            path.Add(node);
+
+            node = new Node(3);
+            path.Add(node);
+
+            node = new Node(4);
+            path.Add(node);
+
+            node = new Node(5);
+            path.Add(node);
+
+            node = new Node(6);
+            path.Add(node);
+
+            node = new Node(7);
+            path.Add(node);
+
+            path[0].AddEdge(1, path[1]);
+            path[0].AddEdge(5, path[2]);
+            path[0].edges.Sort();
+
+            path[1].AddEdge(1, path[3]);
+            path[1].AddEdge(8, path[4]);
+
+            path[2].AddEdge(0, path[3]);
+            path[2].AddEdge(1, path[5]);
+            path[2].edges.Sort();
+
+            path[3].AddEdge(1, path[1]);
+            path[3].AddEdge(0, path[2]);
+            path[3].edges.Sort();
+
+            path[4].AddEdge(6, path[7]);
+            path[4].edges.Sort();
+
+            path[5].AddEdge(1, path[6]);
+            path[5].edges.Sort();
+
+            path[6].AddEdge(1, path[4]);
+            path[6].edges.Sort();
+
+            List<Node> shortestPath = GetShortestPathDijkstra();
+
+            string stringState;
+            int nUserState;
+            int moves = 0;
+
+            Thread thread = new Thread(DFS);
+            thread.Start();
+
+            while (intState != 7)
+            {
+                stringState = IntToStringState(intState);
+                Console.WriteLine("Current state: " + stringState);
+
+                Console.Write("Enter your desired state: ");
+                //userState = Console.ReadLine().ToUpper();
+
+
+                wait = true;
+                while (wait) ;
+
+                nUserState = StringToIntState(userState);
+                Console.WriteLine(userState);
+
+
+                int[] stateList = graphList[intState];
+                bool valid = false;
+
+                if (stateList != null)
+                {
+                    foreach (int n in stateList)
+                    {
+                        if (n == nUserState)
+                        {
+                            valid = true;
+                            intState = nUserState;
+                            ++moves;
+                            break;
+                        }
+                    }
+                }
+
+                if (!valid)
+                {
+                    Console.WriteLine("That is an invalid move.");
+                }
+            }
+
+            Console.WriteLine("You won in " + moves + " moves.");
+
+            thread.Abort();
+        }
+
+        static string IntToStringState(int intState)
+        {
+            string stringState = null;
+
+            switch (intState)
+            {
+                case 0:
+                    stringState = "RED";
+                    break;
+                case 1:
+                    stringState = "BLUE";
+                    break;
+                case 2:
+                    stringState = "GREY";
+                    break;
+                case 3:
+                    stringState = "TEAL";
+                    break;
+                case 4:
+                    stringState = "YELLOW";
+                    break;
+                case 5:
+                    stringState = "ORANGE";
+                    break;
+                case 6:
+                    stringState = "PURPLE";
+                    break;
+                case 7:
+                    stringState = "GREEN";
+                    break;
+                default:
+                    stringState = "RED";
+                    break;
+            }
+
+            return stringState;
+        }
+
+        static int StringToIntState(string sState)
+        {
+            int intState;
+
+            switch (sState)
+            {
+                case "RED":
+                    intState = 0;
+                    break;
+                case "BLUE":
+                    intState = 1;
+                    break;
+                case "GREY":
+                    intState = 2;
+                    break;
+                case "TEAL":
+                    intState = 3;
+                    break;
+                case "YELLOW":
+                    intState = 4;
+                    break;
+                case "ORANGE":
+                    intState = 5;
+                    break;
+                case "PURPLE":
+                    intState = 6;
+                    break;
+                case "GREEN":
+                    intState = 7;
+                    break;
+                default:
+                    intState = 0;
+                    break;
+            }
+
+            return intState;
+        }
+        static void DFS()
+        {
+            bool[] visit = new bool[graphList.Length];
+
+            DFSUtil(intState, visit);
+        }
+        static void DFSUtil(int v, bool[] visit)
+        {
+            while (!wait) ;
+
+            visit[v] = true;
+            userState = IntToStringState(v);
+            // Console.Write(v + " ");
+
+            wait = false;
+
+            int[] neighbors = graphList[v];
+            foreach (int n in neighbors)
+            {
+                if (!visit[n])
+                {
+                    DFSUtil(n, visit);
+                }
+            }
+        }
+
+        static public List<Node> GetShortestPathDijkstra()
+        {
+            DijkstraSearch();
+            List<Node> shortestPath = new List<Node>();
+            shortestPath.Add(path[7]);
+            BuildShortestPath(shortestPath, path[7]);
+            shortestPath.Reverse();
+            return (shortestPath);
+        }
+        
+        static private void BuildShortestPath(List<Node> list, Node node)
+        {
+            if(node.nearestToStart == null)
+            {
+                return;
+            }
+
+            list.Add(node.nearestToStart);
+            BuildShortestPath(list, node.nearestToStart);
+        }
+
+        static private int NodeOrderBy(Node n)
+        {
+            return n.minCostToStart;
+        }
+
+        static private void DijkstraSearch()
+        {
+            Node start = path[0];
+
+            start.minCostToStart = 0;
+            List<Node> prioQueue = new List<Node>();
+            prioQueue.Add(start);
+
+            do
+            {
+                prioQueue.Sort();
+
+                prioQueue = prioQueue.OrderBy(delegate (Node n) { return n.minCostToStart; }).ToList();
+                prioQueue = prioQueue.OrderBy((Node n) => { return n.minCostToStart; }).ToList();
+                prioQueue = prioQueue.OrderBy((n) => { return n.minCostToStart; }).ToList();
+                prioQueue = prioQueue.OrderBy((n) => n.minCostToStart).ToList();
+                prioQueue = prioQueue.OrderBy(n => n.minCostToStart).ToList();
+
+                Node node = prioQueue.First();
+                prioQueue.Remove(node);
+
+                foreach (Edge cnn in node.edges)
+                {
+                    Node childNode = cnn.connectedNode;
+                    if (childNode.visit)
+                    {
+                        continue;
+                    }
+
+                    if (childNode.minCostToStart == int.MaxValue || node.minCostToStart + cnn.cost < childNode.minCostToStart)
+                    {
+                        childNode.minCostToStart = node.minCostToStart + cnn.cost;
+                        childNode.nearestToStart = node;
+                        if (!prioQueue.Contains(childNode))
+                        {
+                            prioQueue.Add(childNode);
+                        }
+                    }
+                }
+                node.visit = true;
+                if (node == path[7])
+                {
+                    return;
+                }
+            } while (prioQueue.Any());
+        }
+    }
+
+        public class Node : IComparable<Node>
+        {
+            public int intState;
+            public List<Edge> edges = new List<Edge>();
+
+            public int minCostToStart;
+            public Node nearestToStart;
+            public bool visit;
+
+            public Node(int intState)
+            {
+                this.intState = intState;
+                this.minCostToStart = int.MaxValue;
+            }
+
+            public void AddEdge(int cost, Node connect)
+            {
+                Edge e = new Edge(cost, connect);
+                edges.Add(e);
+            }
+
+            public int CompareTo(Node n)
+            {
+                return this.minCostToStart.CompareTo(n.minCostToStart);
+            }
+        }
+
+        public class Edge : IComparable<Edge>
+        {
+            public int cost;
+            public Node connectedNode;
+
+            public Edge(int cost, Node connectedNode)
+            {
+                this.cost = cost;
+                this.connectedNode = connectedNode;
+            }
+
+            public int CompareTo(Edge e)
+            {
+                return this.cost.CompareTo(e.cost);
+            }
+        }
+    }
---
+++ Wizard_UT3/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wizard_UT3
+{
+    delegate int sortAges(Wizard one, Wizard two);
+    public class Wizard
+    {
+        public string name;
+        public int age;
+        
+        public Wizard(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comparison<Wizard> sortAge = delegate (Wizard a, Wizard b)
+            {
+                int compare = a.age.CompareTo(b.age);
+                if(compare == 0)
+                {
+                    return a.name.CompareTo(b.name);
+                }
+                return compare;
+            };
+
+            List<Wizard> wList = new List<Wizard>();
+
+            Wizard one = new Wizard("Dante", 54);
+            Wizard two = new Wizard("Nero", 24);
+            Wizard three = new Wizard("V", 30);
+            Wizard four = new Wizard("Vergil", 54);
+            Wizard five = new Wizard("Lady", 52);
+            Wizard six = new Wizard("Trish", 29);
+            Wizard seven = new Wizard("Lucia", 10);
+            Wizard eight = new Wizard("Morrison", 60);
+            Wizard nine = new Wizard("Nico", 21);
+            Wizard ten = new Wizard("Mundus", 1000);
+
+            wList.Add(one);
+            wList.Add(two);
+            wList.Add(three);
+            wList.Add(four);
+            wList.Add(five);
+            wList.Add(six);
+            wList.Add(seven);
+            wList.Add(eight);
+            wList.Add(nine);
+            wList.Add(ten);
+
+            Console.WriteLine("Unsorted: ");
+            foreach(Wizard w in wList)
+            {
+                Console.WriteLine(w.name + ": " + w.age);
+            }
+
+            Console.WriteLine();
+
+            wList.Sort(sortAge);
+
+            Console.WriteLine("Sorted: ");
+            foreach(Wizard w in wList)
+            {
+                Console.WriteLine(w.name + ": " + w.age);
+            }
+        }
+    }
+
+    
+}
